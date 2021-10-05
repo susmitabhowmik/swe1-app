bdc2af47d92eb7ca6285307f07f0031c600672ca
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2021-10-04 21:59:58 -0400

    feat: deploy to heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,3 +1,5 @@
+import django_heroku
+
 """
 Django settings for mysite project.
 
@@ -124,3 +126,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
