dda644f93970975bf65f27adb3b8b776d5f1ce06
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2021-10-18 08:37:39 -0400

    add flake8

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,8 @@
 import datetime
-
 from django.db import models
 from django.contrib import admin
 from django.utils import timezone
 
-# Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
@@ -12,18 +10,15 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
-
     @admin.display(
         boolean=True,
         ordering="pub_date",
-        description="Published recently?",
-    )
+        description="Published recently?")
+
     def was_published_recently(self):
         now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        returnDate = now - datetime.timedelta(days=1) <= self.pub_date <= now
+        return returnDate
 
 
 class Choice(models.Model):
