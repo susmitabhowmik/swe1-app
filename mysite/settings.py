14bb10f8df3fc5fde5056dd69b757cae59f6ead8
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2021-10-20 11:58:31 -0400

    update settings.py to fix testing coverage error

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,4 +1,7 @@
-import django_heroku
+import os
+if '/app' in os.environ['HOME']:
+    import django_heroku
+    django_heroku.settings(locals())
 
 """
 Django settings for mysite project.
@@ -126,5 +129,3 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-django_heroku.settings(locals())
